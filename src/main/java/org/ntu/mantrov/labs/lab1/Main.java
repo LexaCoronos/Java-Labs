1deeef3e6ff2bbbef412105943588b412ed9add0
LexaCoronos <mantir99@gmail.com>
2019-10-08 08:25:21 +0300

    second

--- src/main/java/org/ntu/mantrov/labs/lab1/Main.java
+++ src/main/java/org/ntu/mantrov/labs/lab1/Main.java
@@ -3,42 +3,20 @@ package org.ntu.mantrov.labs.lab1;
 import java.util.Scanner;
 
 public class Main {
-	public final static int UPPER_BOARD_OF_RANGE = 100;
 
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
-		int n = Methods.inputWith(s); // rows
-		int m = Methods.inputHeight(s, n); // columns
+		int n = MatrixCalculator.inputWith(s);
+		int m = MatrixCalculator.inputHeight(s, n);
 
 		int[][] matrix = new int[n][m];
 		
-		boolean errorEnter = false;
-		do {
-			System.out
-					.println("Matrix Fill Method: \n1 - Random numbers \n2 - Use Keyboard");
-			int method = Integer.parseInt(s.next());
+		MatrixCalculator.selectMenu(matrix, s);
 
-			switch (method) {
-			case 1:
-				matrix = Methods.setMatrixRandomNumber(matrix);
-				break;
-			case 2:
-				matrix = Methods.setMatrixUseKeyboard(matrix, s);
-				break;
-			default:
-				errorEnter = true;
-			}
-			if (errorEnter == false) {
-				break;
-			}
-			errorEnter = false;
-			System.out.println("Repeat the menu item selection");
-		} while (true);
-
-		Methods.printMatrix(matrix);
-		Methods.calculateMaximumElement(matrix);
-		Methods.calculateMinimumElement(matrix);
-		Methods.sumElement(matrix);
+		MatrixCalculator.printMatrix(matrix);
+		System.out.println("Maximum Element is: " + MatrixCalculator.calculateMaximumElement(matrix));
+		System.out.println("Minimum Element is: " + MatrixCalculator.calculateMinimumElement(matrix));
+		System.out.println("Sum Element matrix A is: " + MatrixCalculator.sumElement(matrix));
 	}
 }
--- src/main/java/org/ntu/mantrov/labs/lab1/MatrixCalculator.java
+++ src/main/java/org/ntu/mantrov/labs/lab1/MatrixCalculator.java
@@ -3,16 +3,17 @@ package org.ntu.mantrov.labs.lab1;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Methods {
-
+public class MatrixCalculator {
+	public final static int UPPER_BOARD_OF_RANGE = 100;
+	
 	public static int inputWith(Scanner s) {
-		System.out.println("Enter number rows matrix A: ");
+		System.out.println("Enter number of rows matrix A: ");
 		return Integer.parseInt(s.next());
 	}
 
 	public static int inputHeight(Scanner s, int n) {
 		int m = 0;
-		System.out.println("Inter number columns matrix A: ");
+		System.out.println("Enter number of columns matrix A: ");
 		do {
 			m = Integer.parseInt(s.next());
 			if (n != m) {
@@ -35,11 +36,11 @@ public class Methods {
 		}
 	}
 
-	public static int[][] setMatrixRandomNumber(int[][] matrix) {
+	public static int[][] setMatrixRandomNumbers(int[][] matrix, int matrixMaxRangeNumber) {
+		Random r = new Random();
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				Random r = new Random();
-				matrix[i][j] = r.nextInt(Main.UPPER_BOARD_OF_RANGE);
+				matrix[i][j] = r.nextInt(matrixMaxRangeNumber);
 			}
 		}
 		return matrix;
@@ -49,7 +50,7 @@ public class Methods {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
 				do {
-					System.out.println("Inter element A[" + i + "][" + j
+					System.out.println("Enter element A[" + i + "][" + j
 							+ "] = ");
 					try {
 						matrix[i][j] = Integer.parseInt(s.next());
@@ -63,7 +64,7 @@ public class Methods {
 		return matrix;
 	}
 
-	public static void calculateMaximumElement(int[][] matrix) {
+	public static int calculateMaximumElement(int[][] matrix) {
 		int max = matrix[0][0];
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
@@ -72,11 +73,10 @@ public class Methods {
 				}
 			}
 		}
-		System.out.println("Maximum Element is: " + max);
-		return;
+		return max;
 	}
 
-	public static void calculateMinimumElement(int[][] matrix) {
+	public static int calculateMinimumElement(int[][] matrix) {
 		int min = matrix[0][0];
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
@@ -85,19 +85,43 @@ public class Methods {
 				}
 			}
 		}
-		System.out.println("Minimum Element is: " + min);
-		return;
+		return min;
 	}
 
-	public static void sumElement(int[][] matrix) {
+	public static int sumElement(int[][] matrix) {
 		int sum = 0;
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
 				sum += matrix[i][j];
 			}
 		}
-		System.out.println("Sum Element matrix A is: " + sum);
-		return;
+		return sum;
+	}
+
+	public static void selectMenu(int[][] matrix, Scanner s) {
+		boolean errorEnter;
+		do {
+			errorEnter = false;
+			System.out
+					.println("Matrix Fill Method: \n1 - Random numbers \n2 - Use Keyboard");
+			int method = Integer.parseInt(s.next());
+
+			switch (method) {
+			case 1:
+				matrix = MatrixCalculator.setMatrixRandomNumbers(matrix, MatrixCalculator.UPPER_BOARD_OF_RANGE);
+				break;
+			case 2:
+				matrix = MatrixCalculator.setMatrixUseKeyboard(matrix, s);
+				break;
+			default:
+				errorEnter = true;
+			}
+			if (errorEnter == false) {
+				break;
+			}
+			System.out.println("Repeat the menu item selection");
+		} while (true);
+		
 	}
 
 }
